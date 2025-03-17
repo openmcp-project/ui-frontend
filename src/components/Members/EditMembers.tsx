34101063d23e809d7f7cf2b086eb7e9866668e1f
Łukasz Goral <lukasz.goral@sap.com>
2025-03-17 13:37:36 +0100

    Update EditMembers.tsx

--- src/components/Members/EditMembers.tsx
+++ src/components/Members/EditMembers.tsx
@@ -1,34 +1,42 @@
-import { useRef, useState } from "react";
-import { Button, Input, InputDomRef } from "@ui5/webcomponents-react";
-import { MemberTable } from "./MemberTable.tsx";
-import { MemberRoleSelect } from "./MemberRoleSelect.tsx";
-import { ValueState } from "../Shared/Ui5ValieState.tsx";
-import { Member, MemberRoles } from "../../lib/api/types/shared/members";
-import { useTranslation } from "react-i18next";
-import { z } from "zod";
+import { useRef, useState} from "react";
+import {Button, Input, InputDomRef} from "@ui5/webcomponents-react";
+import {MemberTable} from "./MemberTable.tsx";
+import {MemberRoleSelect} from "./MemberRoleSelect.tsx";
+import {ValueState} from "../Shared/Ui5ValieState.tsx";
+import {Member, MemberRoles} from "../../lib/api/types/shared/members";
+import {useTranslation} from "react-i18next";
+import {z} from "zod";
 
 export interface EditMembersProps {
   members: Member[];
   onMemberChanged: (members: Member[]) => void;
 }
-export function EditMembers({ members = [], onMemberChanged }: EditMembersProps) {
+
+export function EditMembers({members = [], onMemberChanged}: EditMembersProps) {
   const emailInput = useRef<InputDomRef>(null);
+  const [valueStateMessage, setValueStateMessage] = useState<string>("");
   const [highlightEmail, setHighlightEmail] = useState<ValueState>("None");
   const [role, setRole] = useState(MemberRoles.viewer);
-  const { t } = useTranslation();
+  const {t} = useTranslation();
 
   const addMember = () => {
+    setValueStateMessage("")
     setHighlightEmail("None");
     if (!emailInput.current) {
       return;
     }
     // Check if the email is already in the list,  highlight as error
-    if (members.find((m) => m.name === emailInput.current!.value) || !z.string().email("This is not a valid email.").safeParse(emailInput.current!.value).success) {
-      
+    if (members.find((m) => m.name === emailInput.current!.value)) {
+      setValueStateMessage("User with this email already exists!")
       setHighlightEmail("Negative");
       return;
     }
-    const newMembers = [...members, { name: emailInput.current.value, roles: [role], kind: "User" }]
+    if (!z.string().email().safeParse(emailInput.current!.value).success) {
+      setValueStateMessage("This is not a valid email.")
+      setHighlightEmail("Negative");
+      return;
+    }
+    const newMembers = [...members, {name: emailInput.current.value, roles: [role], kind: "User"}]
     onMemberChanged(newMembers);
     emailInput.current!.value = "";
   }
@@ -42,16 +50,14 @@ export function EditMembers({ members = [], onMemberChanged }: EditMembersProps)
   }
 
 
-
-
-
   return (
     <>
       <div>
-        <Input id="member-email-input" type="Email" placeholder="Email" ref={emailInput} valueState={highlightEmail} ></Input>
-        <MemberRoleSelect value={role} onChange={changeSelectedRole} />
+        <Input id="member-email-input" type="Email" placeholder="Email" ref={emailInput} valueState={highlightEmail}
+               valueStateMessage={<span>{valueStateMessage}</span>} onChange={()=>{ setHighlightEmail('None')}}></Input>
+        <MemberRoleSelect value={role} onChange={changeSelectedRole}/>
         <Button data-testid="add-member-button" onClick={() => addMember()}>{t('EditMembers.addButton')}</Button>
-        <MemberTable members={members} onDeleteMember={removeMember} />
+        <MemberTable members={members} onDeleteMember={removeMember}/>
       </div>
     </>
   )
