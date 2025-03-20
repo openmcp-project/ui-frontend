202f910682b46e051de6a3119a8e214410bde04d
Łukasz Goral <lukasz.goral@sap.com>
2025-03-20 15:12:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider } from "@ui5/webcomponents-react";
-import { AuthProvider } from "react-oidc-context";
-import { LoadCrateKubeConfig } from "./lib/oidc/crate.ts";
-import { SWRConfig } from "swr";
-import { ToastProvider } from "./context/ToastContext.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from '@ui5/webcomponents-react';
+import { AuthProvider } from 'react-oidc-context';
+import { LoadCrateKubeConfig } from './lib/oidc/crate.ts';
+import { SWRConfig } from 'swr';
+import { ToastProvider } from './context/ToastContext.tsx';
 import { CopyButtonProvider } from './context/CopyButtonContext.tsx';
-import { FrontendConfigProvider, LoadFrontendConfig } from "./context/FrontendConfigContext.tsx";
+import {
+  FrontendConfigProvider,
+  LoadFrontendConfig,
+} from './context/FrontendConfigContext.tsx';
 import '@ui5/webcomponents-react/dist/Assets'; //used for loading themes
-import { DarkModeSystemSwitcher } from "./components/Core/DarkModeSystemSwitcher.tsx";
-import ".././i18n.ts";
-import "./utils/i18n/timeAgo";
-import { useTranslation } from "react-i18next";
+import { DarkModeSystemSwitcher } from './components/Core/DarkModeSystemSwitcher.tsx';
+import '.././i18n.ts';
+import './utils/i18n/timeAgo';
+import { useTranslation } from 'react-i18next';
 
 (async () => {
   try {
     const frontendConfig = await LoadFrontendConfig();
     const authconfig = await LoadCrateKubeConfig(frontendConfig.backendUrl);
 
-    ReactDOM.createRoot(document.getElementById("root")!).render(
+    ReactDOM.createRoot(document.getElementById('root')!).render(
       <React.StrictMode>
         <FrontendConfigProvider config={frontendConfig}>
-          <AuthProvider key={"crate"} {...authconfig}>
+          <AuthProvider key={'crate'} {...authconfig}>
             <ThemeProvider>
               <ToastProvider>
                 <CopyButtonProvider>
@@ -40,15 +43,15 @@ import { useTranslation } from "react-i18next";
             </ThemeProvider>
           </AuthProvider>
         </FrontendConfigProvider>
-      </React.StrictMode>
+      </React.StrictMode>,
     );
   } catch (e) {
     const { t } = useTranslation();
-    console.error("failed to load frontend configuration or kubeconfig", e);
-    ReactDOM.createRoot(document.getElementById("root")!).render(
+    console.error('failed to load frontend configuration or kubeconfig', e);
+    ReactDOM.createRoot(document.getElementById('root')!).render(
       <React.StrictMode>
         <div>{t('main.failedMessage')}=</div>
-      </React.StrictMode>
+      </React.StrictMode>,
     );
   }
 })();
