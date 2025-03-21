fd86660af213e397a1318ce6aed09680f0aab3f3
Łukasz Goral <lukasz.goral@sap.com>
2025-03-21 15:34:51 +0100

    Add validations for create project/workspace dialogs (#11)

--- i18n.ts
+++ i18n.ts
@@ -1,4 +1,4 @@
-import i18n, { loadResources } from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import translationEN from './public/locales/en.json'
---
+++ src/lib/api/validations/schemas.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { Member } from '../types/shared/members.ts';
+import i18n from '../../../../i18n.ts';
+
+const { t } = i18n;
+
+const member = z.custom<Member>();
+
+export const validationSchemaProjectWorkspace = z.object({
+  name: z
+    .string()
+    .min(1, t('validationErrors.required'))
+    .regex(
+      /^(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*$/,
+      t('validationErrors.properFormatting'),
+    )
+    .max(25, t('validationErrors.max25chars')),
+  displayName: z.string().optional(),
+  chargingTarget: z.string().optional(),
+  members: z.array(member).refine((members) => members?.length > 0),
+});
