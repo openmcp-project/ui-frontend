29dfd6e545ed20bc4dda843800a40397ddb8e5d7
Hubert <hubert.szczepanski@sap.com>
2025-04-08 13:41:43 +0200

    flux list display

--- src/components/ControlPlane/FluxList.tsx
+++ src/components/ControlPlane/FluxList.tsx
@@ -3,19 +3,21 @@ import {
   AnalyticalTableColumnDefinition,
   Title,
 } from '@ui5/webcomponents-react';
-import ReactTimeAgo from 'react-time-ago';
 import IllustratedError from '../Shared/IllustratedError.tsx';
 import useResource from '../../lib/api/useApiResource';
-import { FluxGitRepo } from '../../lib/api/types/flux/listGitRepo';
+import { FluxRequest } from '../../lib/api/types/flux/listGitRepo';
 import { FluxKustomization } from '../../lib/api/types/flux/listKustomization';
 import { useTranslation } from 'react-i18next';
+import { timeAgo } from '../../utils/i18n/timeAgo.ts';
+import { ResourceStatusCell } from '../Shared/ResourceStatusCell.tsx';
+import { shortenCommitHash } from '../../lib/api/types/shared/helpers.ts';
 
 export default function FluxList() {
   const {
-    data: repoData,
+    data: gitReposData,
     error: repoErr,
     isLoading: repoIsLoading,
-  } = useResource(FluxGitRepo); //404 if component not enabled
+  } = useResource(FluxRequest); //404 if component not enabled
   const {
     data: kustmizationData,
     error: kustomizationErr,
@@ -24,43 +26,138 @@ export default function FluxList() {
 
   const { t } = useTranslation();
 
-  if (repoErr) {
-    return <IllustratedError error={repoErr} />;
-  }
-  if (kustomizationErr) {
-    return <IllustratedError error={kustomizationErr} />;
+  interface CellData<T> {
+    cell: {
+      value: T | null; // null for grouping rows
+      row: {
+        original?: FluxRows; // missing for grouping rows
+      };
+    };
   }
 
-  const columns: AnalyticalTableColumnDefinition[] = [
+  type FluxRows = {
+    name: string;
+    created: string;
+    status: boolean;
+    statusUptadeTime?: string;
+  };
+
+  if (repoErr) {
+    return (
+      <IllustratedError error={repoErr} title={t('FluxList.noFluxError')} />
+    );
+  }
+  if (kustomizationErr) {
+    return (
+      <IllustratedError
+        error={kustomizationErr}
+        title={t('FluxList.noFluxError')}
+      />
+    );
+  }
+
+  const gitReposColumns: AnalyticalTableColumnDefinition[] = [
     {
       Header: t('FluxList.tableNameHeader'),
-      accessor: 'metadata.name',
+      accessor: 'name',
     },
     {
       Header: t('FluxList.tableStatusHeader'),
-      accessor: 'status.usages',
+      accessor: 'status',
+      Cell: (cellData: CellData<FluxRows['status']>) =>
+        cellData.cell.row.original?.status != null ? (
+          <ResourceStatusCell
+            value={cellData.cell.row.original?.status}
+            transitionTime={
+              cellData.cell.row.original?.statusUptadeTime
+                ? cellData.cell.row.original?.statusUptadeTime
+                : ''
+            }
+          />
+        ) : null,
+    },
+    {
+      Header: t('FluxList.tableVersionHeader'),
+      accessor: 'revision',
     },
     {
       Header: t('FluxList.tableCreatedHeader'),
-      accessor: 'metadata.creationTimestamp',
-      Cell: (props: any) => <ReactTimeAgo date={new Date(props.cell.value)} />,
+      accessor: 'created',
     },
   ];
 
+  const kustomizationsColumns: AnalyticalTableColumnDefinition[] = [
+    {
+      Header: t('FluxList.tableNameHeader'),
+      accessor: 'name',
+    },
+    {
+      Header: t('FluxList.tableStatusHeader'),
+      accessor: 'status',
+      Cell: (cellData: CellData<FluxRows['status']>) =>
+        cellData.cell.row.original?.status != null ? (
+          <ResourceStatusCell
+            value={cellData.cell.row.original?.status}
+            transitionTime={
+              cellData.cell.row.original?.statusUptadeTime
+                ? cellData.cell.row.original?.statusUptadeTime
+                : ''
+            }
+          />
+        ) : null,
+    },
+    {
+      Header: t('FluxList.tableCreatedHeader'),
+      accessor: 'created',
+    },
+  ];
+
+  const gitReposRows: FluxRows[] =
+    gitReposData?.items?.map((item) => {
+      return {
+        name: item.metadata.name,
+        status:
+          item.status.conditions.find((x) => x.type === 'Ready')?.status ===
+          'True',
+        statusUptadeTime: item.status.conditions.find((x) => x.type === 'Ready')
+          ?.lastTransitionTime,
+        revision: shortenCommitHash(item.status.artifact.revision),
+        created: timeAgo.format(new Date(item.metadata.creationTimestamp)),
+      };
+    }) ?? [];
+
+  const kustomizationsRows: FluxRows[] =
+    kustmizationData?.items?.map((item) => {
+      return {
+        name: item.metadata.name,
+        status:
+          item.status.conditions.find((x) => x.type === 'Ready')?.status ===
+          'True',
+        statusUptadeTime: item.status.conditions.find((x) => x.type === 'Ready')
+          ?.lastTransitionTime,
+        created: timeAgo.format(new Date(item.metadata.creationTimestamp)),
+      };
+    }) ?? [];
+
   return (
     <>
-      <Title level="H4">Git Repos</Title>
-      <ConfiguredAnalyticstable
-        columns={columns}
-        isLoading={repoIsLoading}
-        data={repoData ?? []}
-      />
-      <Title level="H4">Kustomizations</Title>
-      <ConfiguredAnalyticstable
-        columns={columns}
-        isLoading={kustomizationIsLoading}
-        data={kustmizationData ?? []}
-      />
+      {' '}
+      <div className="crossplane-table-element">
+        <Title level="H4">{t('FluxList.gitOpsTitle')}</Title>
+        <ConfiguredAnalyticstable
+          columns={gitReposColumns}
+          isLoading={repoIsLoading}
+          data={gitReposRows}
+        />
+      </div>
+      <div className="crossplane-table-element">
+        <Title level="H4">{t('FluxList.kustomizationsTitle')}</Title>
+        <ConfiguredAnalyticstable
+          columns={kustomizationsColumns}
+          isLoading={kustomizationIsLoading}
+          data={kustomizationsRows}
+        />
+      </div>
     </>
   );
 }
