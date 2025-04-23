0f5b05273fcea41b8ac94bed1dcb2a0ea11d73ee
Łukasz Goral <lukasz.goral@sap.com>
2025-04-23 13:08:04 +0200

    fixes

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -14,72 +14,72 @@ import { YamlViewButton } from '../Yaml/YamlViewButton.tsx';
 
 export default function ProjectsList() {
   const navigate = useLuigiNavigate();
-  const { data, error } = useResource(ListProjectNames, {
-    refreshInterval: 3000,
-  });
+  const { data, error } = useResource(ListProjectNames, {});
   if (error) {
     return <IllustratedError error={error} />;
   }
   return (
-    <AnalyticalTable
-      style={{ margin: '12px' }}
-      columns={[
-        {
-          Header: t('ProjectsListView.title'),
-          accessor: 'projectName',
-          Cell: (instance: any) => (
-            <div
-              style={{
-                cursor: 'pointer',
-                width: '100%',
-                color: ThemingParameters.sapLinkColor,
-                fontWeight: 'bold',
-                display: 'flex',
-                justifyContent: 'space-between',
-                gap: '0.5rem',
-                alignItems: 'baseline',
-              }}
-            >
-              {instance.cell.value}
-              <YamlViewButton
-                resourceType={'projects'}
-                resourceName={instance.cell.value}
-              />
-            </div>
-          ),
-        },
-        {
-          Header: 'Namespace',
-          accessor: 'nameSpace',
-          Cell: (instance: any) => (
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'start',
-                gap: '0.5rem',
-                alignItems: 'baseline',
-                width: '100%',
-                cursor: 'pointer',
-              }}
-            >
-              <CopyButton text={instance.cell.value} />
-            </div>
-          ),
-        },
-      ]}
-      data={
-        data?.map((e) => {
-          return {
-            projectName: e,
-            nameSpace: projectnameToNamespace(e),
-          };
-        }) ?? []
-      }
-      onRowClick={(e: any) => {
-        navigate(
-          `/mcp/projects/${data ? [e.detail.row.values.projectName] : ''}`,
-        );
-      }}
-    />
+    <>
+      <AnalyticalTable
+        style={{ margin: '12px' }}
+        columns={[
+          {
+            Header: t('ProjectsListView.title'),
+            accessor: 'projectName',
+            Cell: (instance: any) => (
+              <div
+                style={{
+                  cursor: 'pointer',
+                  width: '100%',
+                  color: ThemingParameters.sapLinkColor,
+                  fontWeight: 'bold',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  gap: '0.5rem',
+                  alignItems: 'baseline',
+                }}
+              >
+                {instance.cell.value}
+                <YamlViewButton
+                  resourceType={'projects'}
+                  resourceName={instance.cell.value}
+                />
+              </div>
+            ),
+          },
+          {
+            Header: 'Namespace',
+            accessor: 'nameSpace',
+            Cell: (instance: any) => (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'start',
+                  gap: '0.5rem',
+                  alignItems: 'baseline',
+                  width: '100%',
+                  cursor: 'pointer',
+                }}
+              >
+                <CopyButton text={instance.cell.value} />
+              </div>
+            ),
+          },
+        ]}
+        data={
+          data?.map((e) => {
+            return {
+              projectName: e,
+              nameSpace: projectnameToNamespace(e),
+            };
+          }) ?? []
+        }
+        onRowClick={(e: any) => {
+          navigate(
+            `/mcp/projects/${data ? [e.detail.row.values.projectName] : ''}`,
+          );
+        }}
+      />
+    </>
   );
 }
