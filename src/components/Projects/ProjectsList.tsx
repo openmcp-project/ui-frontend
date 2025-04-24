4161df2d7ff213929fb31583d490fd231489744a
Łukasz Goral <lukasz.goral@sap.com>
2025-04-24 13:59:48 +0200

    Update ProjectsList.tsx

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -9,65 +9,79 @@ import '@ui5/webcomponents-icons/dist/copy';
 import '@ui5/webcomponents-icons/dist/arrow-right';
 import { ListProjectNames } from '../../lib/api/types/crate/listProjectNames';
 import { t } from 'i18next';
+import { YamlViewButton } from '../Yaml/YamlViewButton.tsx';
+import { useMemo } from 'react';
 
 export default function ProjectsList() {
   const navigate = useLuigiNavigate();
   const { data, error } = useResource(ListProjectNames, {
-    refreshInterval: 3000,
+    // refreshInterval: 3000,
   });
+  const stabilizedData = useMemo(
+    () =>
+      data?.map((projectName) => {
+        return {
+          projectName: projectName,
+          nameSpace: projectnameToNamespace(projectName),
+        };
+      }) ?? [],
+    [data, projectnameToNamespace],
+  );
+  const stabilizedColumns = useMemo(
+    () => [
+      {
+        Header: t('ProjectsListView.title'),
+        accessor: 'projectName',
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Cell: (instance: any) => (
+          <div
+            style={{
+              cursor: 'pointer',
+              width: '100%',
+              color: ThemingParameters.sapLinkColor,
+              fontWeight: 'bold',
+            }}
+          >
+            {instance.cell.value}
+            <YamlViewButton
+              resourceType={'projects'}
+              resourceName={instance.cell.value}
+            />
+          </div>
+        ),
+      },
+      {
+        Header: 'Namespace',
+        accessor: 'nameSpace',
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Cell: (instance: any) => (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'start',
+              gap: '0.5rem',
+              alignItems: 'center',
+              width: '100%',
+              cursor: 'pointer',
+            }}
+          >
+            <CopyButton text={instance.cell.value} />
+          </div>
+        ),
+      },
+    ],
+    [],
+  );
   if (error) {
     return <IllustratedError error={error} />;
   }
+
   return (
     <>
       <AnalyticalTable
         style={{ margin: '12px' }}
-        columns={[
-          {
-            Header: t('ProjectsListView.title'),
-            accessor: 'projectName',
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            Cell: (instance: any) => (
-              <div
-                style={{
-                  cursor: 'pointer',
-                  width: '100%',
-                  color: ThemingParameters.sapLinkColor,
-                  fontWeight: 'bold',
-                }}
-              >
-                {instance.cell.value}
-              </div>
-            ),
-          },
-          {
-            Header: 'Namespace',
-            accessor: 'nameSpace',
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            Cell: (instance: any) => (
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'start',
-                  gap: '0.5rem',
-                  alignItems: 'center',
-                  width: '100%',
-                  cursor: 'pointer',
-                }}
-              >
-                <CopyButton text={instance.cell.value} />
-              </div>
-            ),
-          },
-        ]}
-        data={
-          data?.map((e) => {
-            return {
-              projectName: e,
-              nameSpace: projectnameToNamespace(e),
-            };
-          }) ?? []
-        }
+        columns={stabilizedColumns}
+        data={stabilizedData}
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         onRowClick={(e: any) => {
           navigate(
