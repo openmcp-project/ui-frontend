88c379fc97b6973d7efed2b919ff39160cef432e
Łukasz Goral <lukasz.goral@sap.com>
2025-05-14 11:31:01 +0200

    Update ResourceStatusCell.tsx

--- src/components/Shared/ResourceStatusCell.tsx
+++ src/components/Shared/ResourceStatusCell.tsx
@@ -3,12 +3,16 @@ import { StatusCellProps } from '../../lib/shared/interfaces';
 import { timeAgo } from '../../utils/i18n/timeAgo';
 
 export function ResourceStatusCell({ value, transitionTime }: StatusCellProps) {
+  console.log('transitionTime');
+  console.log(transitionTime);
   return (
     <Icon
       design={value ? 'Positive' : 'Negative'}
       name={value ? 'sys-enter-2' : 'sys-cancel-2'}
       showTooltip={true}
-      accessibleName={timeAgo.format(new Date(transitionTime))}
+      accessibleName={
+        transitionTime ? timeAgo.format(new Date(transitionTime)) : '-'
+      }
     />
   );
 }
