b9f0a434f66bce1ba2de6409a814217970b49e88
Andreas Kienle <andreas.kienle@sap.com>
2025-05-26 10:40:39 +0200

    Rename .test. to .cy.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,11 @@
-import {defineConfig} from "cypress";
+import { defineConfig } from 'cypress';
 
-import {defineConfig as defineViteConfig} from 'vite';
+import { defineConfig as defineViteConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
 const viteConfig = defineViteConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
 });
 export default defineConfig({
   includeShadowDom: true,
@@ -15,15 +13,15 @@ export default defineConfig({
   viewportHeight: 1080,
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
       viteConfig: viteConfig,
     },
-    specPattern: "**/*.test.{js,ts,jsx,tsx}",
+    specPattern: '**/*.cy.{js,ts,jsx,tsx}',
   },
 
   e2e: {
-    baseUrl: "http://localhost:5173/",
+    baseUrl: 'http://localhost:5173/',
     setupNodeEvents() {
       // implement node event listeners here
     },
