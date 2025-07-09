1e752454f8544e40da5485bb6810a36fe0f8917b
Moritz Reich <moritz.reich@sap.com>
2025-07-09 14:34:08 +0200

    style: Update code formatting for consistency and readability & fixed fastify routes

--- server/plugins/http-proxy.js
+++ server/plugins/http-proxy.js
@@ -1,28 +1,28 @@
-import fp from "fastify-plugin";
-import httpProxy from "@fastify/http-proxy";
-import { AuthenticationError } from "./auth-utils.js";
+import fp from 'fastify-plugin';
+import httpProxy from '@fastify/http-proxy';
+import { AuthenticationError } from './auth-utils.js';
 
 function proxyPlugin(fastify) {
   const { API_BACKEND_URL } = fastify.config;
   const { OIDC_CLIENT_ID, OIDC_SCOPES } = fastify.config;
 
   fastify.register(httpProxy, {
-    prefix: "/onboarding",
+    prefix: '/onboarding',
     upstream: API_BACKEND_URL,
     preHandler: async (request, reply) => {
-      request.log.info("Entering HTTP proxy preHandler.");
+      request.log.info('Entering HTTP proxy preHandler.');
 
-      const useCrate = request.headers["x-use-crate"];
+      const useCrate = request.headers['x-use-crate'];
 
-      const keyAccessToken = useCrate ? "onboarding_accessToken" : "mcp_accessToken";
-      const keyTokenExpiresAt = useCrate ? "onboarding_tokenExpiresAt" : "mcp_tokenExpiresAt";
-      const keyRefreshToken = useCrate ? "onboarding_refreshToken" : "mcp_refreshToken";
+      const keyAccessToken = useCrate ? 'onboarding_accessToken' : 'mcp_accessToken';
+      const keyTokenExpiresAt = useCrate ? 'onboarding_tokenExpiresAt' : 'mcp_tokenExpiresAt';
+      const keyRefreshToken = useCrate ? 'onboarding_refreshToken' : 'mcp_refreshToken';
 
       // Check if there is an access token
       const accessToken = request.encryptedSession.get(keyAccessToken);
       if (!accessToken) {
-        request.log.error("Missing access token.");
-        return reply.unauthorized("Missing access token.");
+        request.log.error('Missing access token.');
+        return reply.unauthorized('Missing access token.');
       }
 
       // Check if the access token is expired or about to expire
@@ -30,35 +30,42 @@ function proxyPlugin(fastify) {
       const now = Date.now();
       const REFRESH_BUFFER_MILLISECONDS = 20 * 1000; // to allow for network latency
       if (!expiresAt || now < expiresAt - REFRESH_BUFFER_MILLISECONDS) {
-        request.log.info("Access token is still valid; no refresh needed.");
+        request.log.info('Access token is still valid; no refresh needed.');
         return;
       }
 
-      request.log.info({ expiresAt: new Date(expiresAt).toISOString() }, "Access token is expired or about to expire; attempting refresh.");
+      request.log.info(
+        { expiresAt: new Date(expiresAt).toISOString() },
+        'Access token is expired or about to expire; attempting refresh.',
+      );
 
       // Check if there is a refresh token
       const refreshToken = request.encryptedSession.get(keyRefreshToken);
       if (!refreshToken) {
-        request.log.error("Missing refresh token; deleting encryptedSession.");
-        request.encryptedSession.clear();//TODO: also clear user encrpytion key?
-        return reply.unauthorized("Session expired without token refresh capability.");
+        request.log.error('Missing refresh token; deleting encryptedSession.');
+        request.encryptedSession.clear(); //TODO: also clear user encrpytion key?
+        return reply.unauthorized('Session expired without token refresh capability.');
       }
 
       // Attempt to refresh the tokens
       try {
         const issuerConfiguration = fastify.issuerConfiguration;
 
-        const refreshedTokenData = await fastify.refreshAuthTokens(refreshToken, {
-          clientId: OIDC_CLIENT_ID,
-          scopes: OIDC_SCOPES,
-        }, issuerConfiguration.tokenEndpoint);
+        const refreshedTokenData = await fastify.refreshAuthTokens(
+          refreshToken,
+          {
+            clientId: OIDC_CLIENT_ID,
+            scopes: OIDC_SCOPES,
+          },
+          issuerConfiguration.tokenEndpoint,
+        );
         if (!refreshedTokenData || !refreshedTokenData.accessToken) {
-          request.log.error("Token refresh failed (no access token); deleting session.");
-          request.encryptedSession.clear();//TODO: also clear user encrpytion key?
-          return reply.unauthorized("Session expired and token refresh failed.");
+          request.log.error('Token refresh failed (no access token); deleting session.');
+          request.encryptedSession.clear(); //TODO: also clear user encrpytion key?
+          return reply.unauthorized('Session expired and token refresh failed.');
         }
 
-        request.log.info("Token refresh successful; updating the session.");
+        request.log.info('Token refresh successful; updating the session.');
 
         request.encryptedSession.set(keyAccessToken, refreshedTokenData.accessToken);
         if (refreshedTokenData.refreshToken) {
@@ -67,17 +74,17 @@ function proxyPlugin(fastify) {
           request.encryptedSession.delete(keyRefreshToken);
         }
         if (refreshedTokenData.expiresIn) {
-          const newExpiresAt = Date.now() + (refreshedTokenData.expiresIn * 1000);
+          const newExpiresAt = Date.now() + refreshedTokenData.expiresIn * 1000;
           request.encryptedSession.set(keyTokenExpiresAt, newExpiresAt);
         } else {
           request.encryptedSession.delete(keyTokenExpiresAt);
         }
 
-        request.log.info("Token refresh successful and session updated; continuing with the HTTP request.");
+        request.log.info('Token refresh successful and session updated; continuing with the HTTP request.');
       } catch (error) {
         if (error instanceof AuthenticationError) {
-          request.log.error("AuthenticationError during token refresh: %s", error);
-          return reply.unauthorized("Error during token refresh.");
+          request.log.error('AuthenticationError during token refresh: %s', error);
+          return reply.unauthorized('Error during token refresh.');
         } else {
           throw error;
         }
@@ -85,13 +92,15 @@ function proxyPlugin(fastify) {
     },
     replyOptions: {
       rewriteRequestHeaders: (req, headers) => {
-        const useCrate = req.headers["x-use-crate"];
-        const accessToken = useCrate ? req.encryptedSession.get("onboarding_accessToken") : `${req.encryptedSession.get("onboarding_accessToken")},${req.encryptedSession.get("mcp_accessToken")}`;
+        const useCrate = req.headers['x-use-crate'];
+        const accessToken = useCrate
+          ? req.encryptedSession.get('onboarding_accessToken')
+          : `${req.encryptedSession.get('onboarding_accessToken')},${req.encryptedSession.get('mcp_accessToken')}`;
 
         return {
           ...headers,
           authorization: accessToken,
-        }
+        };
       },
     },
   });
