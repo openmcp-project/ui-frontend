a0be69b932ccb5136e0decf639af610e9e8d72d1
Moritz Reich <moritz.reich@sap.com>
2025-07-25 11:37:08 +0200

    fix(sentry): remove sendDefaultPii option to enhance privacy

--- src/mount.ts
+++ src/mount.ts
@@ -8,9 +8,6 @@ let sentryRoutes = Routes;
 if (import.meta.env.VITE_SENTRY_DSN && import.meta.env.VITE_SENTRY_DSN.length > 0) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
-    // Setting this option to true will send default PII data to Sentry.
-    // For example, automatic IP address collection on events
-    sendDefaultPii: true,
     environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
     integrations: [
       Sentry.reactRouterV7BrowserTracingIntegration({
