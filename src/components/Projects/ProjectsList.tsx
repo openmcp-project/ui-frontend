3c3526270f4becaf8d35cd54e664c38332446bf9
Lasserich <lasse.friedrich@sap.com>
2025-08-20 11:36:57 +0200

    fix: won the linter war

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -133,11 +133,17 @@ export default function ProjectsList() {
 
   return (
     <>
-    <AnalyticalTable
-      style={{maxWidth: '1200px', margin: '10px auto 0px auto', width: '100%', borderRadius: '12px', overflow: 'hidden'}}
-      columns={stabilizedColumns}
-      data={stabilizedData}
-    />    
+      <AnalyticalTable
+        style={{
+          maxWidth: '1200px',
+          margin: '10px auto 0px auto',
+          width: '100%',
+          borderRadius: '12px',
+          overflow: 'hidden',
+        }}
+        columns={stabilizedColumns}
+        data={stabilizedData}
+      />
     </>
   );
 }
