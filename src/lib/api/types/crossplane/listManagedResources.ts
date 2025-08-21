8263c6fbfb9dc3ce0ebdc69fc5d565e58cd66d6e
Lasserich <lasse.friedrich@sap.com>
2025-08-21 09:34:45 +0200

    feat: use new types

--- src/lib/api/types/crossplane/listManagedResources.ts
+++ src/lib/api/types/crossplane/listManagedResources.ts
@@ -1,30 +1,10 @@
+import { ManagedResourceItem } from '../../../shared/types';
 import { Resource } from '../resource';
 
-export type ManagedResourcesResponse = [
-  {
-    items: [
-      {
-        kind: string;
-        metadata: {
-          name: string;
-          creationTimestamp: string;
-          labels: [];
-        };
-        status?: {
-          conditions?: [
-            {
-              type: 'Ready' | 'Synced' | unknown;
-              status: 'True' | 'False';
-              lastTransitionTime: string;
-              message?: string;
-              reason?: string;
-            },
-          ];
-        };
-      },
-    ];
-  },
-];
+export type ManagedResourcesResponse = {
+  items: [ManagedResourceItem];
+}
+  
 
 export const ManagedResourcesRequest: Resource<ManagedResourcesResponse> = {
   path: '/managed',
