a50d24fffbcc949fb95a2ac96fd661c12c23e552
Lasserich <lasse.friedrich@sap.com>
2025-08-29 17:28:26 +0200

    fix: linting

--- src/components/Graphs/Graph.tsx
+++ src/components/Graphs/Graph.tsx
@@ -76,7 +76,11 @@ const Graph: React.FC = () => {
   }
 
   return (
-    <div className={styles.graphContainer} style={{maxWidth: '1280px', margin: '0 auto'}} data-theme={isDarkTheme ? 'dark' : 'light'}>
+    <div
+      className={styles.graphContainer}
+      style={{ maxWidth: '1280px', margin: '0 auto' }}
+      data-theme={isDarkTheme ? 'dark' : 'light'}
+    >
       <div className={styles.graphColumn}>
         <ReactFlow
           data-theme={isDarkTheme ? 'dark' : 'light'}
