7969060a40d6a4cf3f8551bb1a26dafe6b92190a
Łukasz Goral <lukasz.goral@sap.com>
2025-09-29 14:38:15 +0200

    Update removeManagedFieldsProperty.ts

--- src/utils/removeManagedFieldsProperty.ts
+++ src/utils/removeManagedFieldsProperty.ts
@@ -1,36 +1,60 @@
 export type Resource = {
+  apiVersion: string;
   kind: string;
-  items?: {
-    metadata: {
-      name: string;
-      managedFields?: unknown;
-    };
-  }[];
+  items?: Omit<Resource, 'items'>[];
   metadata: {
     name: string;
+    namespace?: string;
+    labels?: Record<string, string>;
+    annotations?: {
+      'kubectl.kubernetes.io/last-applied-configuration'?: string;
+      [key: string]: string | undefined;
+    };
     managedFields?: unknown;
+    creationTimestamp?: string;
+    finalizers?: string[];
+    generation?: number;
+    resourceVersion?: string;
+    uid?: string;
   };
+  spec?: unknown;
+  status?: unknown;
 };
 
-export const removeManagedFieldsProperty = (resourceObject: Resource) => {
-  if (resourceObject?.metadata?.managedFields) {
-    return {
-      ...resourceObject,
-      metadata: {
-        ...resourceObject.metadata,
-        managedFields: undefined,
-      },
-    };
+export const removeManagedFieldsProperty = (resourceObject: Resource, showOnlyImportantData: boolean) => {
+  if (!resourceObject) {
+    return resourceObject;
   }
+  const processMetadata = (metadata: Resource['metadata']) => {
+    const { managedFields, generation, uid, annotations, ...restMetadata } = metadata || {};
+
+    const newAnnotations = { ...annotations };
+    if (showOnlyImportantData) {
+      delete newAnnotations['kubectl.kubernetes.io/last-applied-configuration'];
+    }
+
+    return {
+      ...restMetadata,
+      ...(Object.keys(newAnnotations).length > 0 && {
+        annotations: newAnnotations,
+      }),
+    };
+  };
+
+  const processResource = (resource: Omit<Resource, 'items'>) => {
+    const { metadata, ...restResource } = resource;
+    return {
+      ...restResource,
+      ...(metadata && { metadata: processMetadata(metadata) }),
+    };
+  };
+
   if (resourceObject?.items) {
     return {
       ...resourceObject,
-      items: resourceObject.items.map((item) => ({
-        ...item,
-        metadata: { ...item.metadata, managedFields: undefined },
-      })),
+      items: resourceObject.items.map(processResource),
     };
   }
 
-  return resourceObject;
+  return processResource(resourceObject);
 };
