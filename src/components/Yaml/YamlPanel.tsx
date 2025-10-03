4af57b5d42837b42fce50a7428d165a13cd2d784
Łukasz Goral <lukasz.goral@sap.com>
2025-10-03 11:58:23 +0200

    feat: Changed component to display YAML files to Monaco Editor (#308)

--- src/components/Yaml/YamlPanel.tsx
+++ src/components/Yaml/YamlPanel.tsx
@@ -37,7 +37,7 @@ const YamlPanel: FC<YamlPanelProps> = ({ yamlString, filename }) => {
           </Button>
         )}
       </FlexBox>
-      <YamlViewer yamlString={yamlString} />
+      <YamlViewer yamlString={yamlString} filename={filename} />
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { viteFastify } from '@fastify/vite/plugin';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,14 @@ export default defineConfig({
   plugins: [
     viteFastify({ spa: true }),
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/monaco-editor/min/vs',
+          dest: 'assets',
+        },
+      ],
+    }),
     sentryVitePlugin({
       org: process.env.SENTRY_ORG,
       project: process.env.SENTRY_PROJECT,
