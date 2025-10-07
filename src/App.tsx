21bd296bc0d5f60a5e59c587d63a82e3fcce6da2
Andreas Kienle <andreas.kienle@sap.com>
2025-10-07 12:01:13 +0200

    feat: change favicon (#294)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import AppRouter from './AppRouter';
 import { useAuthOnboarding } from './spaces/onboarding/auth/AuthContextOnboarding.tsx';
 import '@ui5/webcomponents-icons/dist/AllIcons.d.ts';
-import { useEffect } from 'react';
-import { useFrontendConfig } from './context/FrontendConfigContext.tsx';
 import { SignInPage } from './spaces/onboarding/pages/SignInPage/SignInPage.tsx';
 import { BusyIndicator } from '@ui5/webcomponents-react';
 import * as Sentry from '@sentry/react';
 
 function App() {
   const auth = useAuthOnboarding();
-  const frontendConfig = useFrontendConfig();
-
-  useEffect(() => {
-    if (frontendConfig && frontendConfig.landscape) {
-      document.title = `[${frontendConfig.landscape}] MCP`;
-    }
-  }, [frontendConfig]);
 
   if (auth.isLoading) {
     return <BusyIndicator active />;
