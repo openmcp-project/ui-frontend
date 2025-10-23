632ca479332c0cbcca7eec8e56b7df5c7cda5e79
Andreas Kienle <andreas.kienle@sap.com>
2025-10-23 10:50:46 +0200

    refactor

--- src/components/Wizards/CreateManagedControlPlane/CreateManagedControlPlaneWizardContainer.cy.tsx
+++ src/components/Wizards/CreateManagedControlPlane/CreateManagedControlPlaneWizardContainer.cy.tsx
@@ -32,51 +32,50 @@ describe('CreateManagedControlPlaneWizardContainer', () => {
       />,
     );
 
+    const expMutationPayload: CreateManagedControlPlaneType = {
+      apiVersion: 'core.openmcp.cloud/v1alpha1',
+      kind: 'ManagedControlPlane',
+      metadata: {
+        name: 'some-text',
+        namespace: '--ws-',
+        annotations: {
+          'openmcp.cloud/display-name': '',
+        },
+        labels: {
+          'openmcp.cloud.sap/charging-target-type': '',
+          'openmcp.cloud.sap/charging-target': '',
+        },
+      },
+      spec: {
+        authentication: {
+          enableSystemIdentityProvider: true,
+        },
+        components: {
+          apiServer: {
+            type: 'GardenerDedicated',
+          },
+        },
+        authorization: {
+          roleBindings: [
+            {
+              role: 'admin',
+              subjects: [
+                {
+                  kind: 'User',
+                  name: 'openmcp:name@domain.com',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+
     cy.get('#name').find(' input[id*="inner"]').type('some-text');
     cy.get('ui5-button').contains('Next').click(); // navigate to Members
     cy.get('ui5-button').contains('Next').click(); // navigate to Component Selection
     cy.get('ui5-button').contains('Next').click(); // navigate to Summarize
     cy.get('ui5-button').contains('Create').click();
-
-    cy.then(() =>
-      cy.wrap(createMutationPayload).deepEqualJson({
-        apiVersion: 'core.openmcp.cloud/v1alpha1',
-        kind: 'ManagedControlPlane',
-        metadata: {
-          name: 'some-text',
-          namespace: '--ws-',
-          annotations: {
-            'openmcp.cloud/display-name': '',
-          },
-          labels: {
-            'openmcp.cloud.sap/charging-target-type': '',
-            'openmcp.cloud.sap/charging-target': '',
-          },
-        },
-        spec: {
-          authentication: {
-            enableSystemIdentityProvider: true,
-          },
-          components: {
-            apiServer: {
-              type: 'GardenerDedicated',
-            },
-          },
-          authorization: {
-            roleBindings: [
-              {
-                role: 'admin',
-                subjects: [
-                  {
-                    kind: 'User',
-                    name: 'openmcp:name@domain.com',
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      }),
-    );
+    cy.then(() => cy.wrap(createMutationPayload).deepEqualJson(expMutationPayload));
   });
 });
