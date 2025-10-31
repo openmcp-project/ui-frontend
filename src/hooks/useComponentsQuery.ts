b9a20e2ed491f2d369926e252298d0f6b3c05536
Andreas Kienle <andreas.kienle@sap.com>
2025-10-31 10:13:58 +0100

    test: add tests for creating and updating MCP (#352)

---
+++ src/hooks/useComponentsQuery.ts
@@ -0,0 +1,14 @@
+import { useApiResource } from '../lib/api/useApiResource.ts';
+import { APIError } from '../lib/api/error.ts';
+import { ListManagedComponents, ManagedComponentList } from '../lib/api/types/crate/listManagedComponents.ts';
+
+export interface GetComponentsHookResult {
+  components: ManagedComponentList | undefined;
+  error: APIError | undefined;
+  isLoading: boolean;
+}
+export function useComponentsQuery(): GetComponentsHookResult {
+  const { data: components, error, isLoading } = useApiResource(ListManagedComponents(), undefined, true);
+
+  return { components, error, isLoading };
+}
