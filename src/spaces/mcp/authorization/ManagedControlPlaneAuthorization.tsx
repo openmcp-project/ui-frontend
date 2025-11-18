2c331abb0f3862a6e2cd2bfb454ffb4fc65942d3
Łukasz Goral <lukasz.goral@sap.com>
2025-11-18 14:19:39 +0100

    Update ManagedControlPlaneAuthorization.tsx

--- src/spaces/mcp/authorization/ManagedControlPlaneAuthorization.tsx
+++ src/spaces/mcp/authorization/ManagedControlPlaneAuthorization.tsx
@@ -2,10 +2,16 @@ import { ReactNode } from 'react';
 import { useGetMcpUserRights } from './useGetMcpUserRights.ts';
 
 import IllustratedError from '../../../components/Shared/IllustratedError.tsx';
+import { FlexBox } from '@ui5/webcomponents-react';
 
 export const ManagedControlPlaneAuthorization = ({ children }: { children: ReactNode }) => {
   const { isMcpMember } = useGetMcpUserRights();
-  if (!isMcpMember) return <IllustratedError title={'Not member of this mcp'} />;
+  if (!isMcpMember)
+    return (
+      <FlexBox justifyContent={'Center'} alignItems={'Center'} style={{ height: '100%' }}>
+        <IllustratedError title={'You are not a member of this Managed Control Plane'} />
+      </FlexBox>
+    );
 
   return <>{children}</>;
 };
