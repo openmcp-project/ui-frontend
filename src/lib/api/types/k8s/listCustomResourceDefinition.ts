68eaa3dc9d6cb19184c4b187a5221b59124e4b9c
Łukasz Goral <lukasz.goral@sap.com>
2025-11-21 11:18:19 +0100

    Delete listCustomResourceDefinition.ts

--- src/lib/api/types/k8s/listCustomResourceDefinition.ts
+++
@@ -1,7 +0,0 @@
-import { Resource } from '../resource';
-
-//TODO: typing and jq query
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const CustomResourceDefinitions: Resource<any> = {
-  path: '/apis/apiextensions.k8s.io/v1/customresourcedefinitions',
-};
