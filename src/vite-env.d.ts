9ab7042b0c2ef6eadf67166b1ed78a012527807d
Łukasz Goral <lukasz.goral@sap.com>
2025-11-26 11:25:15 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_CYPRESS_TEST?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare global {
   interface Window {
     VITE_CYPRESS_TEST?: string;
