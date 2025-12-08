2b706c893856207dc19ca5cdf61165297748a31f
Łukasz Goral <lukasz.goral@sap.com>
2025-12-08 09:11:25 +0100

    feat: MCP: visualize members and MCP status in header area (#386)

---
+++ src/components/ControlPlanes/McpMembersAvatarView/McpMembersAvatarView.tsx
@@ -0,0 +1,24 @@
+import { MembersAvatarView } from '../List/MembersAvatarView.tsx';
+import { convertRoleBindingsToMembers } from '../../../utils/convertRoleBindingsToMembers.ts';
+import { FlexBox, Text } from '@ui5/webcomponents-react';
+import { useTranslation } from 'react-i18next';
+import styles from './McpMembersAvatarView.module.css';
+
+interface Props {
+  project?: string;
+  workspace?: string;
+  roleBindings?: { role: string; subjects: { kind: string; name: string }[] }[];
+}
+
+export function McpMembersAvatarView({ roleBindings, project, workspace }: Props) {
+  const members = convertRoleBindingsToMembers(roleBindings);
+  const { t } = useTranslation();
+  return (
+    <FlexBox direction="Column">
+      <Text className={styles.membersTitle}>
+        {t('common.members')} ({members.length}):
+      </Text>
+      <MembersAvatarView members={members} project={project} workspace={workspace} hideNamespaceColumn />
+    </FlexBox>
+  );
+}
