4d20d3b084e694a834254144147e617a082bc432
Hubert <hubert.szczepanski@sap.com>
2026-02-15 16:02:19 +0100

    PR changes part 5 apollo client

--- src/spaces/onboarding/services/ApolloClientProvider/ApolloClientProvider.tsx
+++ src/spaces/onboarding/services/ApolloClientProvider/ApolloClientProvider.tsx
@@ -3,28 +3,28 @@ import { ApolloProvider } from '@apollo/client/react';
 import { HttpLink } from '@apollo/client/link/http';
 import { ReactNode } from 'react';
 
+const graphqlUrl = '/api/graphql';
+
+const httpLink = new HttpLink({
+  uri: graphqlUrl,
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-use-crate': 'true',
+    },
+  }));
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
 export function ApolloClientProvider({ children }: { children: ReactNode }) {
-  const graphqlUrl = '/api/graphql';
-
-  const httpLink = new HttpLink({
-    uri: graphqlUrl,
-  });
-
-  const authLink = new ApolloLink((operation, forward) => {
-    operation.setContext(({ headers = {} }) => ({
-      headers: {
-        ...headers,
-        'x-use-crate': 'true',
-      },
-    }));
-
-    return forward(operation);
-  });
-
-  const client = new ApolloClient({
-    link: ApolloLink.from([authLink, httpLink]),
-    cache: new InMemoryCache(),
-  });
-
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
