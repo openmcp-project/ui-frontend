071f33f8adabdca60c08a05c7885093d06d8aa45
Łukasz Goral <lukasz.goral@sap.com>
2026-02-25 15:21:07 +0100

    Update DeprecatedLabel.tsx

--- src/components/Ui/DeprecatedLabel/DeprecatedLabel.tsx
+++ src/components/Ui/DeprecatedLabel/DeprecatedLabel.tsx
@@ -1,29 +1,25 @@
 import { useTranslation } from 'react-i18next';
-import { useRef, useState } from 'react';
-import { Button, ButtonDomRef, Link, PopoverDomRef, ResponsivePopover, Text } from '@ui5/webcomponents-react';
+import { useId, useState } from 'react';
+import { Button, Link, ResponsivePopover, Text } from '@ui5/webcomponents-react';
 import PopoverPlacement from '@ui5/webcomponents/dist/types/PopoverPlacement.js';
 import styles from './DeprecatedLabel.module.css';
 
 export const DeprecatedLabel = () => {
   const { t } = useTranslation();
-  const popoverRef = useRef<PopoverDomRef>(null);
-  const buttonRef = useRef<ButtonDomRef>(null);
+  const buttonId = useId();
   const [open, setOpen] = useState(false);
   const docsUrl = import.meta.env.VITE_MCP2_DOCS_URL;
   const handleClick = () => {
-    if (popoverRef.current) {
-      (popoverRef.current as unknown as { opener: EventTarget | null }).opener = buttonRef.current;
-      setOpen((prev) => !prev);
-    }
+    setOpen((prev) => !prev);
   };
 
   return (
     <>
-      <Button ref={buttonRef} icon={'message-warning'} className={styles.label} onClick={handleClick}>
+      <Button id={buttonId} icon={'message-warning'} className={styles.label} onClick={handleClick}>
         {t('common.deprecated')}
       </Button>
       <ResponsivePopover
-        ref={popoverRef}
+        opener={buttonId}
         open={open}
         placement={PopoverPlacement.Bottom}
         onClose={() => setOpen(false)}
