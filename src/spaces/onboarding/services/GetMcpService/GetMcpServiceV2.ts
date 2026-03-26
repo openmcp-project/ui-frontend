ea9df8647ed302059b15bf3350f53bd47f9ffebd
Łukasz Goral <lukasz.goral@sap.com>
2026-03-26 14:58:47 +0100

    Update GetMcpServiceV2.ts

--- src/spaces/onboarding/services/GetMcpService/GetMcpServiceV2.ts
+++ src/spaces/onboarding/services/GetMcpService/GetMcpServiceV2.ts
@@ -1,81 +1,11 @@
-import { NetworkStatus, gql } from '@apollo/client';
+import { NetworkStatus } from '@apollo/client';
 import { useQuery } from '@apollo/client/react';
-import { ResultOf, TypedDocumentNode, VariablesOf } from '@graphql-typed-document-node/core';
 
+import { graphql } from '../../../../types/__generated__/graphql';
+import { GetMcPv2Query } from '../../../../types/__generated__/graphql/graphql';
 import { ControlPlaneStatusType, ControlPlaneType, ReadyStatus } from '../../../../lib/api/types/crate/controlPlanes';
 
-type RoleRef = {
-  kind?: string | null;
-  name?: string | null;
-  namespace?: string | null;
-};
-
-type Subject = {
-  apiGroup?: string | null;
-  kind?: string | null;
-  name?: string | null;
-  namespace?: string | null;
-};
-
-type RoleBinding = {
-  roleRefs?: (RoleRef | null)[] | null;
-  subjects?: (Subject | null)[] | null;
-};
-
-type Condition = {
-  type?: string | null;
-  status?: string | null;
-  reason?: string | null;
-  message?: string | null;
-  lastTransitionTime?: string | null;
-};
-
-type TokenRule = {
-  apiGroups?: (string | null)[] | null;
-  resources?: (string | null)[] | null;
-  verbs?: (string | null)[] | null;
-};
-
-type Token = {
-  name?: string | null;
-  permissions?: ({ rules?: (TokenRule | null)[] | null } | null)[] | null;
-  roleRefs?: (RoleRef | null)[] | null;
-};
-
-type GetMcpV2Result = {
-  core_openmcp_cloud?: {
-    v2alpha1?: {
-      ManagedControlPlaneV2: {
-        kind?: string | null;
-        metadata?: {
-          name?: string | null;
-          namespace?: string | null;
-          annotations?: unknown;
-          creationTimestamp?: string | null;
-        } | null;
-        spec?: {
-          iam?: {
-            oidc?: {
-              defaultProvider?: { roleBindings?: (RoleBinding | null)[] | null } | null;
-              extraProviders?: ({ roleBindings?: (RoleBinding | null)[] | null } | null)[] | null;
-            } | null;
-            tokens?: (Token | null)[] | null;
-          } | null;
-        } | null;
-        status?: {
-          phase?: string | null;
-          access?: unknown;
-          observedGeneration?: number | null;
-          conditions?: (Condition | null)[] | null;
-        } | null;
-      };
-    } | null;
-  } | null;
-};
-
-type GetMcpV2Variables = { name: string; namespace?: string | null };
-
-const GET_MCP_V2_QUERY: TypedDocumentNode<GetMcpV2Result, GetMcpV2Variables> = gql`
+const GET_MCP_V2_QUERY = graphql(`
   query GetMCPv2($name: String!, $namespace: String) {
     core_openmcp_cloud {
       v2alpha1 {
@@ -154,12 +84,15 @@ const GET_MCP_V2_QUERY: TypedDocumentNode<GetMcpV2Result, GetMcpV2Variables> = g
       }
     }
   }
-`;
+`);
 
-type GetMcpV2Query = ResultOf<typeof GET_MCP_V2_QUERY>;
-type GetMcpV2QueryVariables = VariablesOf<typeof GET_MCP_V2_QUERY>;
-type McpV2Item = NonNullable<NonNullable<GetMcpV2Query['core_openmcp_cloud']>['v2alpha1']>['ManagedControlPlaneV2'];
-type McpV2Condition = NonNullable<NonNullable<NonNullable<McpV2Item>['status']>['conditions']>[number];
+type McpV2Item = NonNullable<NonNullable<GetMcPv2Query['core_openmcp_cloud']>['v2alpha1']>['ManagedControlPlaneV2'];
+type McpV2Condition = NonNullable<NonNullable<McpV2Item['status']>['conditions']>[number];
+type _McpV2DefaultProvider = NonNullable<
+  NonNullable<NonNullable<NonNullable<McpV2Item['spec']>['iam']>['oidc']>['defaultProvider']
+>;
+type RoleBinding = NonNullable<NonNullable<_McpV2DefaultProvider['roleBindings']>[number]>;
+type Subject = NonNullable<NonNullable<RoleBinding['subjects']>[number]>;
 
 function parseAccess(accessData: unknown): ControlPlaneStatusType['access'] {
   if (!accessData) return undefined;
@@ -226,7 +159,7 @@ function mapMcpV2Item(item: McpV2Item): ControlPlaneType | undefined {
 }
 
 export function useGetMcpV2Query(name?: string, namespace?: string) {
-  const queryResult = useQuery<GetMcpV2Query, GetMcpV2QueryVariables>(GET_MCP_V2_QUERY, {
+  const queryResult = useQuery(GET_MCP_V2_QUERY, {
     variables: { name: name ?? '', namespace },
     skip: !name || !namespace,
     notifyOnNetworkStatusChange: true,
