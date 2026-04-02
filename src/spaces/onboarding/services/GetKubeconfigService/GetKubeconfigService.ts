076371ac3d1e7842164909d642e7137d618f6978
Łukasz Goral <lukasz.goral@sap.com>
2026-04-02 13:44:11 +0200

    Update GetKubeconfigService.ts

--- src/spaces/onboarding/services/GetKubeconfigService/GetKubeconfigService.ts
+++ src/spaces/onboarding/services/GetKubeconfigService/GetKubeconfigService.ts
@@ -1,6 +1,10 @@
-import { graphql } from '../../../../types/__generated__/graphql';
+import { NetworkStatus } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
 
-export const GET_KUBECONFIG_QUERY = graphql(`
+import { graphql } from '../../../../types/__generated__/graphql';
+import { GetKubeconfigQuery } from '../../../../types/__generated__/graphql/graphql';
+
+const GET_KUBECONFIG_QUERY = graphql(`
   query GetKubeconfig($kubeConfigName: String!, $namespaceName: String) {
     v1 {
       Secret(name: $kubeConfigName, namespace: $namespaceName) {
@@ -9,3 +13,22 @@ export const GET_KUBECONFIG_QUERY = graphql(`
     }
   }
 `);
+
+type KubeconfigData = NonNullable<NonNullable<GetKubeconfigQuery['v1']>['Secret']>['data'];
+
+export function useGetKubeconfig(kubeConfigName?: string, namespaceName?: string) {
+  const queryResult = useQuery(GET_KUBECONFIG_QUERY, {
+    variables: { kubeConfigName: kubeConfigName ?? '', namespaceName },
+    skip: !kubeConfigName || !namespaceName,
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const isPending = queryResult.networkStatus === NetworkStatus.loading;
+  const data: KubeconfigData = queryResult.data?.v1?.Secret?.data;
+
+  return {
+    data,
+    error: queryResult.error,
+    isPending,
+  };
+}
