7c2c3e37fd726e118265ece47460ab48134fb132
Łukasz Goral <lukasz.goral@sap.com>
2026-04-08 11:43:49 +0200

    Update generate-graphql-types-watch.sh

--- scripts/generate-graphql-types-watch.sh
+++ scripts/generate-graphql-types-watch.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -z "$1" ]; then
-  echo "Usage: npm run generate-graphql-types:watch <access-token>"
+  echo "Usage: npm run generate-graphql-types:watch -- <access-token>"
   exit 1
 fi
 
