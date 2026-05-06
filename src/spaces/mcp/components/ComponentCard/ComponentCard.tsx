211f1812a293b439e907f2903a2b233374c2848f
Łukasz Goral <lukasz.goral@sap.com>
2026-05-06 16:54:11 +0200

    fix
    Signed-off-by: Łukasz Goral <lukasz.goral@sap.com>

--- src/spaces/mcp/components/ComponentCard/ComponentCard.tsx
+++ src/spaces/mcp/components/ComponentCard/ComponentCard.tsx
@@ -1,14 +1,16 @@
-import { Button, Card, CardHeader } from '@ui5/webcomponents-react';
+import { Button, Card, CardHeader, FlexBox } from '@ui5/webcomponents-react';
+import { ObjectStatus } from '@ui5/webcomponents-react/wrappers';
+import { clsx } from 'clsx';
+import { useTranslation } from 'react-i18next';
 import { Kpi, KpiProps } from '../Kpi/Kpi.tsx';
 import styles from './ComponentCard.module.css';
-import { useTranslation } from 'react-i18next';
-import { clsx } from 'clsx';
 
 export type ComponentCardProps = KpiProps & {
   name: string;
   description: string;
   logoImgSrc: string;
   isInstalled: boolean;
+  isV2?: boolean;
   version?: string;
   onNavigateToComponentSection?: () => void;
   onInstallButtonClick?: () => void;
@@ -19,6 +21,7 @@ export function ComponentCard({
   description,
   logoImgSrc,
   isInstalled,
+  isV2 = false,
   version,
   onNavigateToComponentSection,
   onInstallButtonClick,
@@ -43,22 +46,34 @@ export function ComponentCard({
       className={canNavigateToComponentDetails ? styles.cardInteractive : styles.cardNoninteractive}
       onClick={canNavigateToComponentDetails ? onNavigateToComponentSection : undefined}
     >
-      <div
-        className={clsx(
-          styles.content,
-          canNavigateToComponentDetails ? styles.cardInteractive : styles.cardNoninteractive,
+      <div className={styles.cardContent}>
+        {isV2 && isInstalled && (
+          <FlexBox justifyContent="Start" alignItems="End">
+            <ObjectStatus className={styles.status} state="Positive" showDefaultIcon>
+              {t('Kpi.installed')}
+            </ObjectStatus>
+          </FlexBox>
         )}
-      >
-        {isInstalled ? (
-          <div data-cy="kpi-container" className={styles.kpiContainer}>
-            <Kpi {...props} />
-          </div>
-        ) : (
-          <div data-cy="uninstalled-container" className={styles.uninstalledContainer}>
-            {onInstallButtonClick && (
-              <Button data-cy="install-button" icon="sap-icon://add-product" onClick={onInstallButtonClick}>
-                {t('ComponentCard.installButton', { component: name })}
-              </Button>
+
+        {!isV2 && (
+          <div
+            className={clsx(
+              styles.content,
+              canNavigateToComponentDetails ? styles.cardInteractive : styles.cardNoninteractive,
+            )}
+          >
+            {isInstalled ? (
+              <div data-cy="kpi-container" className={styles.kpiContainer}>
+                <Kpi {...props} />
+              </div>
+            ) : (
+              <div data-cy="uninstalled-container" className={styles.uninstalledContainer}>
+                {onInstallButtonClick && (
+                  <Button data-cy="install-button" icon="sap-icon://add-product" onClick={onInstallButtonClick}>
+                    {t('ComponentCard.installButton', { component: name })}
+                  </Button>
+                )}
+              </div>
             )}
           </div>
         )}
