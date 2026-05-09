2b453c32b18ce1c3f10fe72ffba5da94ae0b8e24
Johannes Ott <johannes.ott@sap.com>
2026-05-09 19:39:04 +0200

    refine: update YAML menu item and remove separator lines
    - Add "Show YAML" translation and update menu item text
    - Remove blue gradient separator lines from metadata cards per feedback
    - Keep cleaner card layout without visual dividers
    
    Signed-off-by: Johannes Ott <johannes.ott@sap.com>

--- src/components/ControlPlanes/ControlPlanePageMenu.tsx
+++ src/components/ControlPlanes/ControlPlanePageMenu.tsx
@@ -41,7 +41,7 @@ export const ControlPlanePageMenu: FC<ControlPlanesListMenuProps> = ({
       >
         <MenuItem key={'edit'} text={t('ControlPlaneCard.editMCP')} data-action="editMcp" icon="edit" />
         {onYamlClick && (
-          <MenuItem key={'yaml'} text={t('yaml.YAML')} data-action="yaml" icon="document-text" />
+          <MenuItem key={'yaml'} text={t('yaml.showYAML')} data-action="yaml" icon="document-text" />
         )}
       </Menu>
     </>
