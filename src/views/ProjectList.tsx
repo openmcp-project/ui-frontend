4559d791f9e8b2943b5202b9471d0d0e6f0044fe
Johannes Ott <johannes.ott@sap.com>
2026-06-12 08:14:04 +0200

    feat: add subheader with learn more link to project list page
    Adds a descriptive subheader and a "Learn more" link to the docs on the
    project list page so users have context on projects and workspaces.
    
    Signed-off-by: Johannes Ott <johannes.ott@sap.com>

--- src/views/ProjectList.tsx
+++ src/views/ProjectList.tsx
@@ -1,8 +1,10 @@
-import { ObjectPage, ObjectPageSection, ObjectPageTitle } from '@ui5/webcomponents-react';
+import { Link, ObjectPage, ObjectPageSection, ObjectPageTitle } from '@ui5/webcomponents-react';
+import { useTranslation } from 'react-i18next';
 import ProjectsList from '../components/Projects/ProjectsList.tsx';
 import { BreadcrumbFeedbackHeader } from '../components/Core/BreadcrumbFeedbackHeader.tsx';
 import { ProjectListToolbar } from '../components/Projects/ProjectListToolbar.tsx';
-import { useTranslation } from 'react-i18next';
+
+const LEARN_MORE_URL = 'https://open-control-plane.io/users/concepts/projects-and-workspaces/';
 
 export default function ProjectsListView() {
   const { t } = useTranslation();
@@ -13,6 +15,14 @@ export default function ProjectsListView() {
       titleArea={
         <ObjectPageTitle
           header={t('ProjectsListView.pageTitle')}
+          subHeader={
+            <span>
+              {t('ProjectsListView.subHeader')}{' '}
+              <Link href={LEARN_MORE_URL} target="_blank">
+                {t('ProjectsListView.learnMore')}
+              </Link>
+            </span>
+          }
           breadcrumbs={<BreadcrumbFeedbackHeader />}
           actionsBar={<ProjectListToolbar />}
         />
