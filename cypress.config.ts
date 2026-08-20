598661f7c1c99c20b517615af961dc3498fe6f76
Łukasz Goral <lukasz.goral@sap.com>
2026-08-20 10:53:13 +0200

    Update cypress.config.ts
    Signed-off-by: Łukasz Goral <lukasz.goral@sap.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -28,6 +28,7 @@ const viteConfig = defineViteConfig({
 
 export default defineConfig({
   includeShadowDom: true,
+  retries: { runMode: 2, openMode: 0 },
   viewportWidth: 1920,
   viewportHeight: 1080,
   component: {
